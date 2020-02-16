09929f0ed7a595b5ee2f29a06be7014e1652dbf9
João Pedro Caires <joaopdante@gmail.com>
2020-02-16 13:27:52 -0300

    Update login

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
-import {useUser} from './context/auth'
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 
 ReactDOM.render(
-    <Main />,
+    <Root />,
     document.getElementById('root')
 );
 
 function Root() {
-    const user = useUser()
-    return user ? <Main /> : <Login />
+    /*const user = useUser()
+    return user ? <Main /> : <Login />*/
+    return (
+    <Login />
+    );
 }
