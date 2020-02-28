ad25d488d485285c557c2f1b3a139fb1203359eb
João Pedro Caires <joaopdante@gmail.com>
2020-02-28 06:00:52 -0300

    Update code

--- src/pages/Server/Server.js
+++ src/pages/Server/Server.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { useLocation, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import api from "../../context/api";
@@ -7,29 +7,33 @@ import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import ProgressBar from "react-bootstrap/ProgressBar";
- 
-function useQuery() {
-  return new URLSearchParams(useLocation().search);
-}
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 
 class Server extends Component {
   state = {
     server: {},
     cpu: 0,
-    ram: 0
+    ram: 0,
+    log: "none",
+    cmd: ""
   };
 
   componentDidMount() {
     let query = new URLSearchParams(this.props.location.search);
     const id = query.get('id')
     this.getServer(id);
-    /*this.interval = setInterval(() => {
+    this.interval = setInterval(() => {
       api.post("/api/getstats", { id: id })
       .then(result => {
         if(result.data.success) {
-          this.setState({ 
-            cpu: result.data.cpu,
-            ram: result.data.ram
+          this.setState({
+            cpu: result.data.info.cpu,
+            ram: result.data.info.ram,
+            log: result.data.info.log,
+            count: result.data.info.count,
+            max: result.data.info.max,
+            online: result.data.info.online
            })
         }
       })
@@ -37,7 +41,7 @@ class Server extends Component {
         console.log(err);
         return null;
       });
-    }, 2500);*/
+    }, 1000);
   }
 
   componentWillUnmount() {
@@ -52,7 +56,13 @@ class Server extends Component {
     api.post("/api/getserver", { id: id })
     .then(result => {
       this.setState({
-        server: result.data.server
+        server: result.data.server,
+        cpu: 0,
+        ram: 0,
+        log: "",
+        count: result.data.server.count,
+        max: result.data.server.max,
+        online: result.data.server.online
       });
     })
     .catch(err => {
@@ -64,9 +74,6 @@ class Server extends Component {
   startServer = () => () => {
     api.post("/api/start", { id: this.state.server.id })
     .then(result => {
-      this.setState({
-        server: result.data.server
-      });
     })
     .catch(err => {
       console.log(err);
@@ -77,9 +84,6 @@ class Server extends Component {
   stopServer = () => () => {
     api.post("/api/stop", { id: this.state.server.id })
     .then(result => {
-      this.setState({
-        server: result.data.server
-      });
     })
     .catch(err => {
       console.log(err);
@@ -90,9 +94,6 @@ class Server extends Component {
   restartServer = () => () => {
     api.post("/api/restart", { id: this.state.server.id })
     .then(result => {
-      this.setState({
-        server: result.data.server
-      });
     })
     .catch(err => {
       console.log(err);
@@ -103,14 +104,16 @@ class Server extends Component {
   killServer = () => () => {
     api.post("/api/kill", { id: this.state.server.id })
     .then(result => {
-      this.setState({
-        server: result.data.server
-      });
     })
     .catch(err => {
       console.log(err);
       return null;
     });
+  }
+
+  sendCommand = async e => {
+    e.preventDefault();
+    const result = await api.post("/api/command", { id: this.state.server.id, cmd: this.state.cmd });
   }
 
   render() {
@@ -135,23 +138,31 @@ class Server extends Component {
             </tr>
             <tr>
               <td className="right-content">Status:</td>
-              <td><FontAwesomeIcon icon={this.state.server.online ? faCheckCircle : faTimesCircle} color={this.state.server.online ? "#25c025" : "#ff2f2f"}/></td>
+              <td><FontAwesomeIcon icon={this.state.online ? faCheckCircle : faTimesCircle} color={this.state.online ? "#25c025" : "#ff2f2f"}/></td>
             </tr>
             <tr>
               <td className="right-content">Players:</td>
-              <td>{this.state.server.count} / {this.state.server.max}</td>
+              <td>{this.state.count} / {this.state.max}</td>
             </tr>
             <tr>
               <td className="right-content">Min Memory:</td>
-              <td>{this.state.server.minmemory}</td>
+              <td>{this.state.server.minmemory} MB</td>
             </tr>
             <tr>
               <td className="right-content">Max Memory:</td>
-              <td>{this.state.server.maxmemory}</td>
+              <td>{this.state.server.maxmemory} MB</td>
             </tr>
             <tr>
               <td className="right-content">Filename:</td>
               <td>{this.state.server.filename}</td>
+            </tr>
+            <tr>
+              <td className="right-content">Arguments:</td>
+              <td>{this.state.server.arguments}</td>
+            </tr>
+            <tr>
+              <td className="right-content">Directory:</td>
+              <td>{this.state.server.directory}</td>
             </tr>
             <tr>
               <td className="right-content">CPU Usage:</td>
@@ -163,8 +174,25 @@ class Server extends Component {
             </tr>
           </tbody>
         </Table>
+        <h5 className="content-header">Console</h5>
+        <hr/>
+        <form onSubmit={this.sendCommand}>
+        <InputGroup className="mb-3">
+          <FormControl 
+          placeholder="Command"
+          aria-label="Command"
+          aria-describedby="basic-addon2"
+          name="cmd"
+          id="cmd"
+          onChange={e => this.setState({ cmd: e.target.value })}
+          />
+          <InputGroup.Append>
+            <Button type="submit" variant="outline-secondary">Send</Button>
+          </InputGroup.Append>
+        </InputGroup>
+        </form>
         <div className="console">
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+          {this.state.log}
         </div>
       </Container>
     );
