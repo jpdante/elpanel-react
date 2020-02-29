00b899aeb51eaa7611b2cf0267c445daa4878aad
João Pedro Caires <joaopdante@gmail.com>
2020-02-29 19:18:30 -0300

    Update api.js

--- src/context/api.js
+++ src/context/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "http://eagles.fightcraft.com.br:8080/",
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
